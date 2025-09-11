1800a5f9cd7d532eba668e53c813e7e42d87141e
taylor01andgit config --global user.email taylor01anderson@icloud.com <taylor01anderson@icloud.com>
2025-09-10 19:19:32 -0700

    created helper script to test on anvil

--- foundry-fund-me-f23/script/DeployFundMe.s.sol
+++ foundry-fund-me-f23/script/DeployFundMe.s.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.18;
 
 import {FundMe} from "../src/FundMe.sol";
 import {Script} from "../lib/forge-std/src/Script.sol";
+import {HelperConfig} from "./Helperconfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe){
+        // Helper config for testing
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig.priceFeed;
+
         vm.startBroadcast();
+        // Mock
         FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
         return fundMe;
---
+++ foundry-fund-me-f23/script/Helperconfig.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator";
+
+// Fix Magic numbers
+uint8 constant DECIMALS = 8;
+int256 constant INITIAL_ANSWER = 2000e8;
+
+// Script to deploy mock configuration for anvil and others if wanted
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        // if statement to check what chain we are on, 11155111 is sepolia
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getAnvilEthConfig() {
+        vm.startBroadcast();
+        MockV3Aggregator mockV3Aggregator = new MockV3Aggregator(DECIMALS, INITIAL_ANSWER);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockV3Aggregator)
+        });
+        return anvilConfig;
+    }
+
+    function getSepliaEthConfig() public pure returns (NetworkConfig memory) {
+        // insert Sepolia config here
+    }
+}
